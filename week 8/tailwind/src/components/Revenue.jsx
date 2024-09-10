899a1c7aa07bce8c6ce691f23be6347f51eae7cf
Harshawardhan Kamble <kambleharsh08@outlook.com>
2024-09-10 19:23:28 +0530

    final touches

--- week 8/tailwind/src/components/Revenue.jsx
+++ week 8/tailwind/src/components/Revenue.jsx
@@ -1,5 +1,5 @@
 import Table from "./Table"
-
+import { FaRegCircleQuestion } from "react-icons/fa6";
 const Revenue = ({ data }) => {
 
     return (
@@ -11,7 +11,10 @@ const Revenue = ({ data }) => {
             data.map((individual,index) => {
                 return (
                     <div key={index} className={`shadow-md m-6 h-32 p-4 rounded-md  w-80 ${individual.title==="Next Payout"?"bg-blue-900 text-white":"bg-white"}  `} >
+                        <div className="flex gap-3 ">
                         <p >{individual.title}</p>
+                        <FaRegCircleQuestion className="mt-1.5"/>
+                        </div>
                         <div className="mt-8 flex justify-between">
                             <div className="font-bold text-2xl ">{individual?.payment}</div>
                             <div className={`ml-4 underline cursor-pointer  ${individual?.orders==="23 Orders"?"text-white":"text-blue-900"}  `}>{individual?.orders}</div>
